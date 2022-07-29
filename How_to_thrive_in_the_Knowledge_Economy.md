99142e9bc3866ce1bb15475421f809f3d63f7645
Alexander Osipov <shuraosipov@gmail.com>
2022-07-28 21:52:40 -0400

    initial code

---
+++ How_to_thrive_in_the_Knowledge_Economy.md
@@ -0,0 +1,18 @@
+## What is the knowledge economy?
+
+
+The knowledge economy is an economic system that is based on the production and distribution of knowledge and information. The knowledge economy is characterized by high levels of investment in human capital and in research and development, and by the use of knowledge and information to create new products and services.
+
+## Summary: How to be successfull in the knowledge economy?
+
+
+There is no one-size-fits-all answer to this question, as success in the knowledge economy depends on a variety of factors, ranging from an individual's skillset and abilities to the specific industry they are operating in. However, there are a few key things that individuals can do to increase their chances of success in the knowledge economy, including:
+
+1. Developing in-demand skills and abilities: In order to be successful in the knowledge economy, individuals need to possess the skills and abilities that are in high demand by employers. This means staying up-to-date with the latest trends and developments in their field, and continuously learning new skills and knowledge.
+
+2. Being adaptable and flexible: The knowledge economy is constantly changing and evolving, so those who are successful in this environment are those who are able to adapt and change with it. This means being open to new ideas and approaches, and being able to quickly learn and implement new technologies and methods.
+
+3. Being entrepreneurial: Individuals who are successful in the knowledge economy are often those who are able to think outside the box and who are comfortable taking risks. They are often entrepreneurial in nature, and are always on the lookout for new opportunities.
+
+4. Having a global perspective: The knowledge economy is increasingly global in nature, so those who are successful in this environment are those who have a global perspective. This means being able to operate effectively in multiple cultures and understanding the needs and preferences of customers from around the world.
+
---
+++ script.py
@@ -0,0 +1,81 @@
+import openai
+import os
+
+
+ARTICLE_NAME="How to thrive in the Knowledge Economy?" 
+ARTICLE_NAME_WITH_UNDERSCORE = ARTICLE_NAME.replace(" ", "_").replace("?", "")
+
+# QUESTIONS = [
+#     "What is the knowledge economy?",
+#     "What is the purpose of the knowledge economy?",
+#     "How to thrive in the knowledge economy?",
+#     "How to make money in the knowledge economy?",
+#     "How to stay independent in the knowledge economy?",
+#     "How to make a good life in the knowledge economy?",
+#     "What are the backwards-looking issues in the knowledge economy?",
+#     "Summary: How to be successfull in the knowledge economy?",
+# ]
+
+QUESTIONS = {
+    "What is the knowledge economy?":[],
+    # "How to thrive in the knowledge economy?":[],
+    # "How to make money in the knowledge economy?":[],
+    # "How to stay independent in the knowledge economy?":[],
+    # "How to make a good life in the knowledge economy?":[],
+    # "What are the backwards-looking issues in the knowledge economy?":[],
+    "Summary: How to be successfull in the knowledge economy?":[],
+}
+
+"""
+A function to call openai to generate a text response to a question.
+"""
+def get_answser(question, max_tokens=1000, temperature=0.8):
+    # Get the answer from openai
+    openai.api_key = os.environ['OPENAI_API_KEY']
+    response = openai.Completion.create(engine='text-davinci-002', prompt=question,  max_tokens=max_tokens, temperature=temperature)
+    return response.choices[0].text
+
+"""
+A function to iterate over a list of questions, get the answers and save answer to them to the output.
+"""
+def print_answers(questions):
+    for question in questions:
+        answer = get_answser(question)
+        # print(question)
+        # print(answer)
+        # print()
+        questions[question].append(answer)
+
+"""
+A function that iterates overa a dictionary of questions and answers and save outout to a *.MD file.
+Each questions should be on a new line formatter as the second largest heading.
+Each answer should be on a new line formatter as regular text.
+"""
+
+
+"""
+A function that creates a markdown file with the questions and answers.
+Where each question is on a new line formatter as the second largest heading.
+And each answer is on a new line formatter as regular text.
+"""
+def create_markdown_file(questions, output_file_name):
+    with open(output_file_name, "w") as file:
+        for question in questions:
+            file.write("## " + question + "\n")
+            for answer in questions[question]:
+                file.write(answer + "\n")
+            file.write("\n")
+    
+
+
+
+# print(QUESTIONS[0])
+# print(get_answser(QUESTIONS[0]))
+
+print_answers(QUESTIONS)
+
+create_markdown_file(QUESTIONS, f'{ARTICLE_NAME_WITH_UNDERSCORE}.md')
+
+
+
+
