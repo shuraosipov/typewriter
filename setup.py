8c9734998b0c758d79cda839fa740cb4e5abece3
Alexander Osipov <shuraosipov@gmail.com>
2022-08-19 20:07:05 -0400

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="typewriter-cli",
-    version="0.1",
+    version="0.1.0",
     py_modules=["typewriter"],
     include_package_data=True,
     install_requires=["click", "openai"],
