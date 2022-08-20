4f3dbcba0ef5796ed17c16cfd874cc922053bae1
Alexander Osipov <shuraosipov@gmail.com>
2022-08-19 20:15:53 -0400

    better options and commands descriptions

--- typewriter.py
+++ typewriter.py
@@ -17,31 +17,35 @@ def cli(ctx):
     ctx.obj = Canvas()
 
 @cli.command()
-@click.option('--title', required=True, help='Title of the article')
-@click.option('--input_file', required=True, help='Path to input_file containing a list of questions')
-@click.option('--output_file', required=True, help='Path to output_file')
-@click.option('--options', required=False, default=3, help='Number of answers to generate')
+@click.option('--title', required=True, help='The title of the article.')
+@click.option('--input_file', required=True, help='Path to a file containing a list of questions')
+@click.option('--output_file', required=True, help='Path to a canvas json file.')
+@click.option('--options', required=False, default=3, help='Number of answers to generate. Default is 3.')
 @pass_canvas
 def create_canvas(canvas, title, input_file, output_file, options):
+    """ Create a canvas json file from a list of questions. """
     canvas.create_canvas(title, input_file, output_file, options)
 
 @cli.command()
 @click.option('--canvas_file', required=True, help='Path to a canvas json file.')
 @pass_canvas
 def show_canvas(canvas, canvas_file):
+    """ Show content of a canvas json file. """
     canvas.show_canvas(canvas_file)
 
 @cli.command()
 @click.option('--canvas_file', required=True, help='Path to a canvas json file.')
-@click.option('--question_number', required=True, help='A question number to add a new answer to.')
+@click.option('--question_number', required=True, help='A question id to add an answer to.')
 @pass_canvas
 def add_new_answer(canvas, canvas_file, question_number):
+    """ Add a new answer to a question. """
     canvas.add_new_answer(canvas_file, question_number)
 
 @cli.command()
 @click.option('--canvas_file', required=True, help='Path to a canvas json file.')
 @pass_canvas
 def save_article(canvas, canvas_file):
+    """ Save an article to a markdown file. """
     canvas = canvas.read_json_file(canvas_file)
     article = MarkdownFile(canvas)
     article.save_file()
